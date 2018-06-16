de98fd053244b22e08a960f89e6eaeb0169ecef0
Pride Haveit <pride.haveit@gmail.com>
2018-06-16 18:09:15 +0200

    Starting game if anywhere is clicked.

--- core/src/de/basedow/keno/ballonbuster/BalloonMenu.kt
+++ core/src/de/basedow/keno/ballonbuster/BalloonMenu.kt
@@ -4,8 +4,6 @@ import com.badlogic.gdx.Game
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
-import com.badlogic.gdx.scenes.scene2d.InputEvent
-import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.badlogic.gdx.scenes.scene2d.ui.Label
 
 class BalloonMenu(game: Game) : BaseScreen(game) {
@@ -20,16 +18,13 @@ class BalloonMenu(game: Game) : BaseScreen(game) {
         val startGameLabel = Label("Click here to start", style)
         startGameLabel.setFontScale(4f)
         startGameLabel.setPosition(100f, 250f)
-        startGameLabel.addListener(object : InputListener() {
-            override fun touchDown(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int): Boolean {
-                game.screen = BalloonLevel(game)
-                return true
-            }
-        })
         uiStage.addActor(startGameLabel)
     }
 
-    override fun update(delta: Float) {
+    override fun update(delta: Float) {}
 
+    override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+        game.screen = BalloonLevel(game)
+        return true
     }
 }
